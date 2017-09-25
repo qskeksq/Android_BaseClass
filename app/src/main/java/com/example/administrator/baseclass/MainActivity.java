b91fa2d2e7daa6c71fe52a38013d573c4679e4cd
Administrator <qskeksq@gmail.com>
2017-09-25 21:12:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/administrator/baseclass/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.administrator.baseclass;
+
+public class MainActivity extends BaseActivity {
+
+
+
+    @Override
+    void init() {
+        setContentView(R.layout.activity_main);
+    }
+}
